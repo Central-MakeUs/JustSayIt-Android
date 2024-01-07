a55dbacc83747f44c7e1dc5626ce7c6e1d9552f5
Kim Gimun <kmkim7575@gmail.com>
2024-01-07 21:43:05 +0900

    :arrow_up: Chore: update compile sdk to 34
    git push origin main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.sowhat.justsayit"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sowhat.justsayit"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
