dbcf0ad86f1c8241f75592efd3265eefabe5af2b
Kim Gimun <kmkim7575@gmail.com>
2024-01-08 00:02:59 +0900

    :heavy_plus_sign: Chore: add fundamental dependencies for project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
