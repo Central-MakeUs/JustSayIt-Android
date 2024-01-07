ba342bb5d57bd39a8d19c966a43e5c71f2efd96c
Kim Gimun <kmkim7575@gmail.com>
2024-01-08 01:30:22 +0900

    :package: Chore: add build-logic module for multi-module architecture

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,20 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+}
