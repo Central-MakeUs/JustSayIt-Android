4f740dc7ac299880fd896e93c0ecde49e4fe71a0
Kim Gimun <kmkim7575@gmail.com>
2024-01-09 01:21:54 +0900

    :lipstick: Design: add gray, black, white colors for compose

---
+++ core/designsystem/src/main/java/com/sowhat/designsystem/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.sowhat.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray5 = Color(0xFFFAFAFA)
+val Gray10 = Color(0xFFF5F5F5)
+val Gray20 = Color(0xFFEEEEEE)
+val Gray30 = Color(0xFFE0E0E0)
+val Gray40 = Color(0xFFBDBDBD)
+val Gray50 = Color(0xFF9E9E9E)
+val Gray60 = Color(0xFF757575)
+val Gray70 = Color(0xFF616161)
+val Gray80 = Color(0xFF424242)
+val Gray90 = Color(0xFF222222)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
