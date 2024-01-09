aa782029c6e1a0db04a401670ea3a3eba208366c
Kim Gimun <kmkim7575@gmail.com>
2024-01-09 17:26:13 +0900

    :lipstick: Design: add typography types for text in application

--- core/designsystem/src/main/java/com/sowhat/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/sowhat/designsystem/theme/Theme.kt
@@ -1,2 +1,9 @@
 package com.sowhat.designsystem.theme
 
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.sowhat.designsystem.R
+
