a726d4f77707cf7d3051d0c9260a9c8d7d795f31
Kim Gimun <kmkim7575@gmail.com>
2024-01-10 21:49:57 +0900

    :recycle: Refactor: separating textfield composable function into small components

--- core/designsystem/src/main/java/com/sowhat/designsystem/component/TextField.kt
+++ core/designsystem/src/main/java/com/sowhat/designsystem/component/TextField.kt
@@ -8,19 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.Placeholder
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,12 +40,12 @@ fun DefaultTextField(
         verticalArrangement = Arrangement.spacedBy(JustSayItTheme.Spacing.spaceExtraSmall)
     ) {
         DefaultTextFieldTitle(title)
-        DefaultTextfieldContent(value, onValueChange, placeholder)
+        DefaultTextFieldContent(value, onValueChange, placeholder)
     }
 }
 
 @Composable
-private fun DefaultTextfieldContent(
+private fun DefaultTextFieldContent(
     value: String,
     onValueChange: (String) -> Unit,
     placeholder: String
