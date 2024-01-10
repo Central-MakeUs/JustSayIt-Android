f6ce65bddfa7229ecf7a890a01eb0a4f414d5a35
Kim Gimun <kmkim7575@gmail.com>
2024-01-10 21:51:26 +0900

    :sparkles: Feat: implement user config screen using appbar, profile, textfield components

--- authentication/authentication-presentation/src/main/java/com/sowhat/presentation/common/Constants.kt
+++ authentication/authentication-presentation/src/main/java/com/sowhat/presentation/common/Constants.kt
@@ -4,3 +4,4 @@ const val APP_NAME = "그냥, 그렇다고"
 const val APP_INTRO = "(서비스 한줄 소개)"
 
 const val SIGN_IN = "간편 로그인"
+const val COMPLETE = "완료"
--- authentication/authentication-presentation/src/main/java/com/sowhat/presentation/configuration/UserConfigScreen.kt
+++ authentication/authentication-presentation/src/main/java/com/sowhat/presentation/configuration/UserConfigScreen.kt
@@ -1,13 +1,83 @@
 package com.sowhat.presentation.configuration
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sowhat.designsystem.component.AppBar
+import com.sowhat.designsystem.component.DefaultTextField
+import com.sowhat.designsystem.theme.Gray200
+import com.sowhat.designsystem.theme.Gray400
+import com.sowhat.presentation.common.COMPLETE
+import com.sowhat.presentation.component.ProfileImage
 
 @Composable
 fun UserConfigRoute() {
-    UserConfigScreen()
+    var id by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    UserConfigScreen(
+        id = id,
+        onIdChange = { changedId -> id = changedId }
+    )
 }
 
 @Composable
-fun UserConfigScreen() {
+fun UserConfigScreen(
+    modifier: Modifier = Modifier,
+    id: String,
+    onIdChange: (String) -> Unit
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            AppBar(
+                title = null,
+                navigationIcon = null,
+                actionText = COMPLETE
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            ProfileImage(
+                profileUri = "https://github.com/kmkim2689/Android-Wiki/assets/101035437/88d7b249-ad72-4be9-8d79-38dc942e0a7f",
+                badgeDrawable = com.sowhat.designsystem.R.drawable.ic_camera_24,
+                badgeBackgroundColor = Gray200,
+                badgeIconTint = Gray400,
+                onClick = {}
+            )
 
+            DefaultTextField(
+                title = "아이디",
+                placeholder = "2자 이상 12자 이하",
+                value = id,
+                onValueChange = onIdChange
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UserConfigScreenPreview() {
+    var id by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    UserConfigScreen(
+        id = id,
+        onIdChange = { changedId -> id = changedId }
+    )
 }
