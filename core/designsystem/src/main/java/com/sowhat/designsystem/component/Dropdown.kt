6d4b32c2b6e92f0f7c5efd1b65fe6c0c3d2b0ac9
Kim Gimun <kmkim7575@gmail.com>
2024-01-12 00:13:56 +0900

    :lipstick: Design: update the border and background of dropdown menu

--- core/designsystem/src/main/java/com/sowhat/designsystem/component/Dropdown.kt
+++ core/designsystem/src/main/java/com/sowhat/designsystem/component/Dropdown.kt
@@ -1,44 +1,84 @@
 package com.sowhat.designsystem.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sowhat.designsystem.common.normalShadow
+import androidx.compose.ui.unit.dp
+import com.sowhat.designsystem.theme.Gray300
 import com.sowhat.designsystem.theme.JustSayItTheme
 
 @Composable
 fun Dropdown(
-    isExpanded: Boolean,
+    isVisible: Boolean,
     items: List<DropdownItem>,
-    onDismiss: () -> Unit,
+    onDismiss: () -> Unit
 ) {
-    DropdownMenu(
-        modifier = Modifier
-            .background(JustSayItTheme.Colors.subBackground)
-            .normalShadow(),
-        expanded = isExpanded,
-        onDismissRequest = {
-            onDismiss()
-        },
+    // https://stackoverflow.com/questions/66781028/jetpack-compose-dropdownmenu-with-rounded-corners
+    MaterialTheme(
+        shapes = MaterialTheme.shapes.copy(extraSmall = RoundedCornerShape(12.dp))
     ) {
-        items.forEach { dropdownItem ->
-            DropdownMenuItem(
-                text = {
-                    Text(
-                        text = dropdownItem.title,
-                        style = JustSayItTheme.Typography.body3
-                    )
-                },
-                onClick = {
-                    dropdownItem.onItemClick()
-                    onDismiss()
-                }
-            )
+        DropdownMenu(
+            modifier = Modifier
+                .background(
+                    color = JustSayItTheme.Colors.subBackground,
+                )
+                .border(
+                    width = 1.dp,
+                    color = Gray300,
+                    shape = JustSayItTheme.Shapes.medium
+                ),
+            expanded = isVisible,
+            onDismissRequest = {
+                onDismiss()
+            },
+        ) {
+            DropdownMenus(items = items, onDismiss = onDismiss)
         }
+    }
+}
+
+@Composable
+fun ColumnScope.DropdownMenus(
+    items: List<DropdownItem>,
+    onDismiss: () -> Unit
+) {
+    items.forEachIndexed { index, dropdownItem ->
+        if (index != 0) Divider(
+            thickness = JustSayItTheme.Spacing.spaceTiny,
+            color = Gray300,
+        )
+
+        DropdownMenuItem(
+            modifier = Modifier
+                .background(
+                    color = JustSayItTheme.Colors.subBackground,
+                )
+                .padding(vertical = 0.dp),
+            text = {
+                Text(
+                    text = dropdownItem.title,
+                    style = JustSayItTheme.Typography.body3
+                )
+            },
+            onClick = {
+                dropdownItem.onItemClick()
+                onDismiss()
+            }
+        )
     }
 }
 
@@ -50,11 +90,17 @@ data class DropdownItem(
 @Preview(showBackground = true, backgroundColor = 0xffffffff)
 @Composable
 fun DropdownPreview() {
+
+    var isVisible by remember {
+        mutableStateOf(true)
+    }
+
     Dropdown(
-        isExpanded = true,
+        isVisible = isVisible,
         items = listOf(
             DropdownItem(title = "Label 1", onItemClick = {}),
             DropdownItem(title = "Label 2", onItemClick = {}),
         ),
-        onDismiss = {})
+        onDismiss = { isVisible = false }
+    )
 }
