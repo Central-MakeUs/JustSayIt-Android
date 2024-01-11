3dc4b6b1bdd68a979fdb07adb701deb622678af5
Kim Gimun <kmkim7575@gmail.com>
2024-01-11 19:44:48 +0900

    :heavy_plus_sign: Chores: add dependencies for network, database and paging

--- build-logic/convention/src/main/kotlin/com/sowhat/convention/CommonLibraries.kt
+++ build-logic/convention/src/main/kotlin/com/sowhat/convention/CommonLibraries.kt
@@ -5,11 +5,33 @@ import org.gradle.kotlin.dsl.dependencies
 
 internal fun Project.configureCommonLibraries() {
     val libs = extensions.libs
+
+    pluginManager.apply {
+        apply("kotlinx-serialization")
+        apply("org.jetbrains.kotlin.kapt")
+        apply("org.jetbrains.kotlin.plugin.serialization")
+        apply("kotlin-kapt")
+    }
+
 //    Extensions
     dependencies {
-        add("implementation", libs.findLibrary("coil.compose").get())
-        add("implementation", libs.findLibrary("androidx.compose.navigation").get())
-        add("implementation", libs.findLibrary("androidx.compose.navigation.test").get())
-        add("implementation", libs.findLibrary("androidx.navigation.common.ktx").get())
+        implementation(libs, "coil.compose")
+        implementation(libs, "androidx.compose.navigation")
+        implementation(libs, "androidx.compose.navigation.test")
+        implementation(libs, "androidx.navigation.common.ktx")
+
+        implementation(libs, "okhttp.logging")
+        implementation(libs, "retrofit.core")
+        implementation(libs, "retrofit.kotlin.serialization")
+
+
+        implementation(libs, "room.core")
+        implementation(libs, "room.ktx")
+        implementation(libs, "room.paging")
+        kapt(libs, "room.compiler")
+
+
+        implementation(libs, "paging.compose")
+        implementation(libs, "paging.runtime.ktx")
     }
 }
