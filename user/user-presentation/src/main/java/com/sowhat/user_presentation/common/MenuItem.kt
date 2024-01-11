1ce2ae0d9d737b81327710511baf3c94ae7b814e
Kim Gimun <kmkim7575@gmail.com>
2024-01-11 13:13:02 +0900

    :recycle: Refactor: add multiple constructors for MenuItem data class

--- user/user-presentation/src/main/java/com/sowhat/user_presentation/common/MenuItem.kt
+++ user/user-presentation/src/main/java/com/sowhat/user_presentation/common/MenuItem.kt
@@ -1,9 +1,59 @@
 package com.sowhat.user_presentation.common
 
 data class MenuItem(
-    val leadingIcon: Int? = null,
-    val title: String,
-    val trailingIcon: Int? = null,
-    val trailingText: String? = null,
-    val onClick: (() -> Unit)? = null
-)
+    var leadingIcon: Int?,
+    var title: String,
+    var trailingIcon: Int?,
+    var trailingText: String?,
+    var onClick: (() -> Unit)? = null
+) {
+    constructor(
+        title: String,
+        trailingIcon: Int,
+        onClick: (() -> Unit)? = null
+    ): this(
+        leadingIcon = null,
+        title = title,
+        trailingIcon = trailingIcon,
+        trailingText = null,
+        onClick = onClick,
+    )
+
+    constructor(
+        title: String,
+        trailingText: String,
+        onClick: (() -> Unit)? = null
+    ): this(
+        leadingIcon = null,
+        title = title,
+        trailingIcon = null,
+        trailingText = trailingText,
+        onClick = onClick,
+    )
+
+    constructor(
+        leadingIcon: Int,
+        title: String,
+        trailingText: String,
+        onClick: (() -> Unit)? = null
+    ): this(
+        leadingIcon = leadingIcon,
+        title = title,
+        trailingIcon = null,
+        trailingText = trailingText,
+        onClick = onClick,
+    )
+
+    constructor(
+        leadingIcon: Int,
+        title: String,
+        trailingIcon: Int,
+        onClick: (() -> Unit)? = null
+    ): this(
+        leadingIcon = leadingIcon,
+        title = title,
+        trailingIcon = trailingIcon,
+        trailingText = null,
+        onClick = onClick,
+    )
+}
