131bdc2b75416f973b4927697f4e89b481b06f41
Kim Gimun <kmkim7575@gmail.com>
2024-01-29 13:54:53 +0900

    :lipstick: Design: update date format for railed background

--- report/report-presentation/src/main/java/com/sowhat/report_presentation/common/Helper.kt
+++ report/report-presentation/src/main/java/com/sowhat/report_presentation/common/Helper.kt
@@ -1,3 +1,14 @@
 package com.sowhat.report_presentation.common
 
-fun String.toDate() = this.split("T").first()
+import android.util.Log
+
+fun String.toDate(): String = try {
+    this.split("T")
+        .first()
+        .split("-")
+        .mapIndexed { index, date -> if (index == 0) date.slice(IntRange(2, 3)) else date }
+        .joinToString(".")
+} catch (e: Exception) {
+    Log.e("toDate", "error : ${e.message}")
+    "server error"
+}
