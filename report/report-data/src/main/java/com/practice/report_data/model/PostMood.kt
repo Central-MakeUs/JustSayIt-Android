12e3eef86d88654777e0b82bcc3badf7d510064e
Kim Gimun <kmkim7575@gmail.com>
2024-02-05 00:18:19 +0900

    :sparkles: Feat: add api service functions for today mood tracker

---
+++ report/report-data/src/main/java/com/practice/report_data/model/PostMood.kt
@@ -0,0 +1,10 @@
+package com.practice.report_data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostMood(
+    @SerialName("moodCode")
+    val moodCode: String
+)
